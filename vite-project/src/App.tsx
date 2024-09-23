cc5338338d1a552757450167312c599a90759945
Daniel Tremură <tremuradaniel@gmail.com>
2024-09-23 07:51:29 +0300

    advanced imports - glob patterns

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,12 @@
 import './App.css'
 
-const logoName = "ttc-logo";
-// top level await - not supported in every web browser
-const module = await import(`./png-images/${logoName}.png`) as { default: string };
+const modules = import.meta.glob<{default: string}>(`./png-images/*.png`, { eager: true });
 
 function App() {
   return (
     <>
       <div>
-        <img src={module.default} />
+        {Object.values(modules).map(src => <img src={src.default} />)}
       </div>
       <h1>Vite + React</h1>
       <a href="/another-route/">Another route</a>
